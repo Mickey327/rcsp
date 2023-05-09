888344b6c37590ce5ec04b4ed4177e434afcd8b4
mickey327 <usp2002@mail.ru>
2023-05-09 17:33:30 +0300

    Updated Dockerfile + trying to communicate with backend

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,13 +3,22 @@ import ProductList from "../components/product-list/ProductList";
 import {Col, Container, Row} from "react-bootstrap";
 import Typebar from "../components/typebar/Typebar";
 import Companybar from "../components/companybar/Companybar";
+import {checkHello} from "../http/userAPI";
+import Button from "react-bootstrap/Button";
 
 const Shop = () => {
+    const helloCheck = async () => {
+        const response = await checkHello()
+        console.log(response)
+        return response
+    }
+
     return (
         <Container fluid>
             <Container fluid="xxl">
                 <Row className="mb-2">
                     <Col md={3} className="mt-2">
+                        <Button onClick={helloCheck}></Button>
                         <Typebar/>
                         <Companybar/>
                     </Col>
@@ -18,7 +27,6 @@ const Shop = () => {
                     </Col>
 
                 </Row>
-
             </Container>
         </Container>
     );
